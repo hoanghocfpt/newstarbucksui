a4e4e421b7328ad300c05f62f5b3f20e99196f30
tranthimynhi <Tranmynhitk@gmail.com>
2024-06-02 17:14:32 +0700

    ban 2
    còn sửa

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-
+import { IProduct } from '../iproduct';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
- 
+  listProduct: IProduct[] = [];
+  ngOnInit(): void {
+    fetch(` https://665aef83003609eda45f4d1a.mockapi.io/products`)
+      .then(res => res.json())
+      .then(data => {
+        this.listProduct = data;
+      })
+  }
+
 }
