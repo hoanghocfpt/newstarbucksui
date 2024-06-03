d86c30c5de40b758450a1af9a7ad47c468ebbdc2
nguyenphongphu1234 <134608320+PhuNguyenPhong5524@users.noreply.github.com>
2024-06-03 15:11:48 +0700

    update

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-
+  constructor(private productService:ProductService, private router:ActivatedRoute){}
+  product:any = {}
+  id: any
+  ngOnInit():void{
+    this.id = this.router.snapshot.paramMap.get('id')
+    this.productService.getProductById(this.id).subscribe(data => {
+      this.product = data
+      console.log(this.product);
+    })
+  }
 }
