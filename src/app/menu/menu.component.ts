8001d7d3a2e667c9251a3147ef0110c747e1b819
trannghia204 <tranhuunghia204@gmail.com>
2024-06-04 16:17:00 +0700

    update

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
-import { IProduct } from '../iproduct';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../product.service';
+import { CategoriesService, } from '../categories.service';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ICategories } from '../icategories';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
-  listProduct: any[] = []
-  constructor(private productService:ProductService, private router:ActivatedRoute){}
-  ngOnInit():void{
+export class MenuComponent implements OnInit {
+  listProduct: any[] = [];
+  listCategories: any[] = [];
+
+  constructor(
+    private productService: ProductService,
+    private categoriesService: CategoriesService,
+    private router: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
     this.productService.getProducts().subscribe(data => {
-      this.listProduct = data
-    })
+      this.listProduct = data;
+    });
+
+    this.categoriesService.getCategories().subscribe(data => {
+      this.listCategories = data as ICategories[];
+    });
+
   }
+
+   
+
 }
